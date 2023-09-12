0e0379cf105f5efcef090a71baf1ef76d82bfc04
Thomas <thomas-commits@sauvage.pm>
2023-09-12 15:31:25 +0200

    Refactor

---
+++ website/src/pages/Home/Intro.tsx
@@ -0,0 +1,59 @@
+import { Badge, Card, CardHeader, Flex, Image } from "@chakra-ui/react";
+import { Section } from "../../components/Section";
+import { TextL } from "../../utils/Language";
+
+export const Intro = () => {
+  return (
+    <Flex justifyContent="center" flexDirection={{ base: "column", xl: "row" }}>
+      <Section>
+        <Flex
+          flexDirection={{ base: "column", md: "row" }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Image
+            src="/photo.jpg"
+            alt="Thomas Sauvage"
+            boxSize="14em"
+            borderRadius="full"
+          />
+          <TextL margin="2em">
+            {{
+              fr: "Bonjour, je m’appelle Thomas Sauvage, j'ai 21 ans. Je suis étudiant à l’Ecole polytechnique, passionné d’informatique depuis mes 11 ans et de sciences depuis toujours. J’aime comprendre et résoudre les problèmes que je rencontre, et améliorer la qualité des outils que j’utilise au quotidien. J'ai entièrement construit ce site internet avec React, Typescript et Chakra UI.",
+              en: "Good morning, my name is Thomas Sauvage, I'm 21 years old. I'm a student at Ecole polytechnique, passionate about computer science since I was 11 and about science since forever. I like to understand and solve the problems I encounter, and improve the quality of the tools I use everyday. I built this website with React, Typescript and Chakra UI.",
+            }}
+          </TextL>
+        </Flex>
+      </Section>
+
+      <Card background="accent" margin="1em">
+        <CardHeader>
+          <Badge
+            variant="subtle"
+            backgroundColor="veryAccent"
+            color="white"
+            padding="0.5em"
+          >
+            <TextL>
+              {{
+                fr: "Recherche de stage • Juin - Fin août 2024",
+                en: "Internship • June - End of August 2024",
+              }}
+            </TextL>
+          </Badge>
+        </CardHeader>
+        <TextL
+          marginRight="2em"
+          marginLeft="2em"
+          marginBottom="2em"
+          color="white"
+        >
+          {{
+            fr: "Je suis actuellement à la recherche d'un stage de 3 mois dans le cadre de ma deuxième année à l'Ecole polytechnique. Je suis ouvert à toute proposition dans les domaines de l'informatique: dévelopement web, intelligence artificielle, simulation... N'hésitez pas à me contacter.",
+            en: "I am currently looking for a 3-month internship as part of my second year at Ecole polytechnique. I am open to any proposal in the field of computer science: web development, artificial intelligence, simulation... Do not hesitate to contact me.",
+          }}
+        </TextL>
+      </Card>
+    </Flex>
+  );
+};
---
+++ website/src/pages/Home/Skills.tsx
@@ -0,0 +1,162 @@
+import { Link, List } from "@chakra-ui/react";
+import { useContext } from "react";
+import { AccordionLine } from "../../components/AccordionLine";
+import { LItem, LItemL } from "../../components/List";
+import { Section } from "../../components/Section";
+import { LanguageContext, TextL, getText } from "../../utils/Language";
+
+export const Skills = () => {
+  const { language } = useContext(LanguageContext);
+
+  return (
+    <Section
+      title={getText({ fr: "Mes compétences", en: "My skills" }, language)}
+    >
+      <AccordionLine
+        title={getText(
+          { fr: "Programmation", en: "Computer programming" },
+          language
+        )}
+      >
+        <List>
+          <LItemL>
+            {{
+              fr: "Programmation depuis 2013 (mes 11 ans, en MS-DOS)",
+              en: "Programming since 2013 (when I was 11 years old, in MS-DOS)",
+            }}
+          </LItemL>
+          <LItemL marginLeft="3em">
+            {{
+              fr: "en Python depuis 2018 (mes 15 ans)",
+              en: "in Python since 2018 (when I was 15 years old)",
+            }}
+          </LItemL>
+          <LItemL marginLeft="3em">
+            {{
+              fr: "en Ocaml depuis 2020",
+              en: "in Ocaml since 2020",
+            }}
+          </LItemL>
+          <LItemL marginLeft="3em">
+            {{
+              fr: "en Java depuis 2023",
+              en: "in Java since 2023",
+            }}
+          </LItemL>
+          <LItemL>
+            {{
+              fr: "Développement de sites internet en HTML&CSS avec gestion du backend en Django (Python)",
+              en: "Websites development in HTML&CSS with a Django (Python) backend",
+            }}
+          </LItemL>
+          <LItem>
+            <TextL as="span">
+              {{
+                fr: "Développement de sites internet en Typescript React pour le ",
+                en: "Websites development in Typescript React for the ",
+              }}
+            </TextL>
+            <Link
+              href="https://br.binets.fr"
+              color="veryAccent"
+              target="_blank"
+            >
+              Binet Réseau
+            </Link>
+            <TextL as="span">
+              {{
+                fr: " (association d'informatique de l'Ecole Polytechnique)",
+                en: " (computer science association of Ecole Polytechnique)",
+              }}
+            </TextL>
+          </LItem>
+          <LItemL>
+            {{
+              fr: "Cybersécurité : Cryptographie asymétrique & symétrique, hachage, utilisation d’un gestionnaire de mot de passe et d’une clef physique (yubikey)",
+              en: "Cybersecurity: Asymmetric & symmetric cryptography, hashing, use of a password manager and a physical key (yubikey)",
+            }}
+          </LItemL>
+        </List>
+      </AccordionLine>
+
+      <AccordionLine title={getText({ fr: "Technique", en: "Tech" }, language)}>
+        <List>
+          <LItemL>
+            {{
+              fr: "Électronique & informatique : Création et réparation d’ordinateurs, de clavier ergonomiques…",
+              en: "Electronics & computer science: Creation and repair of computers, ergonomic keyboards…",
+            }}
+          </LItemL>
+          <LItemL>
+            {{
+              fr: "Impression 3D",
+              en: "3D printing",
+            }}
+          </LItemL>
+          <LItemL>
+            {{
+              fr: "Permis de pilote de drones",
+              en: "Drone pilot license",
+            }}
+          </LItemL>
+        </List>
+      </AccordionLine>
+
+      <AccordionLine
+        title={getText({ fr: "Artistique", en: "Artistic" }, language)}
+      >
+        <List>
+          <LItemL>
+            {{
+              fr: "Photographie : Portrait, paysage, astrophotographie…",
+              en: "Photography: Portrait, landscape, astrophotography…",
+            }}
+          </LItemL>
+          <LItemL>
+            {{
+              fr: "Retouche photo : Photoshop",
+              en: "Photo editing: Photoshop",
+            }}
+          </LItemL>
+          <LItemL>
+            {{
+              fr: "Montage vidéo : Première pro",
+              en: "Video editing: Premiere pro",
+            }}
+          </LItemL>
+          <LItemL>
+            {{
+              fr: "Guitare",
+              en: "Guitar",
+            }}
+          </LItemL>
+        </List>
+      </AccordionLine>
+
+      <AccordionLine
+        title={getText({ fr: "Langues", en: "Languages" }, language)}
+      >
+        <List>
+          <LItemL>
+            {{
+              fr: "Langue maternelle: Français",
+              en: "Native language: French",
+            }}
+          </LItemL>
+          <LItemL>
+            {{
+              fr: "Maîtrise (C1/C2): Anglais",
+              en: "Proficient: English",
+            }}
+          </LItemL>
+          <LItemL>
+            {{
+              fr: "Avancé (B1/B2): Espagnol",
+              en: "Advanced: Spanish",
+            }}
+          </LItemL>
+        </List>
+      </AccordionLine>
+    </Section>
+  );
+};
