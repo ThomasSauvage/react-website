d39f1895761e64680e3bc2f3bb2d533eda11c0a5
Thomas Sauvage <thomas.sauvage@polytechnique.edu>
2025-05-15 21:19:20 +0200

    fix(build): reverting to old tsconfig

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./App";
 import { LanguageProvider } from "./utils/Language";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
