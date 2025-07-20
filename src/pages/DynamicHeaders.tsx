d03fed365ebdc2df46bb2a5cfe28a4a9d4b6cb63
Thomas Sauvage <thomas.sauvage@polytechnique.edu>
2025-07-20 22:37:38 +0200

    feat: Add hreflang for international SEO

--- src/pages/DynamicHeaders.tsx
+++ src/pages/DynamicHeaders.tsx
@@ -1,7 +1,9 @@
 import { useContext } from "react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import { getText, LanguageContext } from "../utils/Language";
+import { getText, LanguageContext, languages } from "../utils/Language";
 import { introduction } from "../utils/intro";
+
+const URL = "https://sauvage.pm/";
 
 export const DynamicHeaders = () => {
   const { language } = useContext(LanguageContext);
@@ -9,7 +11,17 @@ export const DynamicHeaders = () => {
   return (
     <HelmetProvider>
       <Helmet>
+        <html lang={language} />
         <meta name="description" content={getText(introduction, language)} />
+        {languages.map((lang) => (
+          <link
+            rel="alternate"
+            key={lang}
+            hrefLang={lang}
+            href={`${URL}${lang}/`}
+          />
+        ))}
+        <link rel="alternate" hrefLang="x-default" href={URL} />
       </Helmet>
     </HelmetProvider>
   );
